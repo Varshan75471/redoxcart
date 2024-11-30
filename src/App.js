6ba89f90a2f62207c6df72f954484adaf779711a
Varshan <varshan5velan@gmail.com>
2024-11-30 11:28:00 +0530

    Modified App.js to include task functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import CartItem from './components/CartItem'; // Update the path based on your project
+import CartSummary from './components/CartSummary';
+import './App.css'; // Ensure this is styled correctly
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const items = useSelector((state) => state.cart.items);
+
+    return (
+        <div className="app">
+            <h1>React Redux Cart</h1>
+            <div className="cart">
+                <div className="cart-items">
+                    {items.map((item) => (
+                        <CartItem key={item.id} item={item} />
+                    ))}
+                </div>
+                <CartSummary />
+            </div>
+        </div>
+    );
+};
 
 export default App;
